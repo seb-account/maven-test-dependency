e50334741e582bcd46696a0946aec5cb67cb4c9e
Sebastian R <sebray1234@gmail.com>
2017-12-22 16:16:24 +0100

    Initial commit

---
+++ src/test/java/com/example/jenkins/MessageSourceTest.java
@@ -0,0 +1,12 @@
+package com.example.jenkins;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class MessageSourceTest {
+    @Test
+    public void test(){
+        fail("Not implemented");
+    }
+}
